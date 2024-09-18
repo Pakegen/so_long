2679362c9f8fd10a8c847bc2f8c2a60903081d52
Lanila <109435712+Pakegen@users.noreply.github.com>
2024-09-18 15:07:22 +0200

    Add files via upload

--- so_long/src/testcaca.c
+++ so_long/src/testcaca.c
@@ -6,13 +6,14 @@
 /*   By: quenalla <quenalla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 13:58:42 by quenalla          #+#    #+#             */
-/*   Updated: 2024/09/18 14:24:42 by quenalla         ###   ########.fr       */
+/*   Updated: 2024/09/18 15:04:53 by quenalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 #include<fcntl.h>
-#include"./mlx.h"
+#include"so_long.h"
+#include"mlx/mlx.h"
 #include<X11/X.h>
 #include<X11/keysym.h>
 
@@ -22,9 +23,13 @@ void	test(void)
 	void	*mlx_win;
 
 	mlx = mlx_init();
+	if (!mlx)
+		return (1);
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	if (!mlx_win)
+		return (1);
 	mlx_loop(mlx);
-
+	return (0);
 }
 
 int	main(void)
